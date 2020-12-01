60ac1be5a33692298affa3b0558411aeadcab6d0
Andreea Pădurariu <62242417+Andreeaxhx@users.noreply.github.com>
2020-12-01 16:22:34 +0200

    Update grep.py

--- grep.py
+++ grep.py
@@ -10,14 +10,17 @@ def file_search(regex, face_match, path, ignore_case, count): #regex=expresia re
                                           #face_match=valoare booleana setata in functie de argumentul NOT;
                                           #path_to_file=path ul catre file ul in care vrem sa se caute regex
     match=False #presupunem ca regex nu face match cu niciun cuvant din file
-
+    nr_of_matches=0
 
     if os.path.isfile(path):
         file=open(path)
         print("-----", path)
         lines=file.readlines() #se citeste file ul linic cu linie
         for line in lines:
-            matches_of_regex=re.findall(regex, line)
+            if ignore_case:
+                matches_of_regex=re.findall(regex, line, flags=re.IGNORECASE)
+            else:
+                matches_of_regex = re.findall(regex, line)
             if matches_of_regex:
                 match=True
                 if face_match==True:
@@ -27,6 +30,9 @@ def file_search(regex, face_match, path, ignore_case, count): #regex=expresia re
                         line=lista[0]+colored(i, 'red')+lista[1]
                     line=line.replace("\n", "")
                     print(line)
+                    nr_of_matches+=1
+        if count:
+            print(colored("Number of matches:", "green"), nr_of_matches)
         if match==False and face_match==False:
             print("regex nu face match in file")
 
@@ -35,24 +41,30 @@ def file_search(regex, face_match, path, ignore_case, count): #regex=expresia re
         for f in os.listdir(path):
 
             if os.path.isfile(path+"\\"+f):
-                file_search(regex, face_match, path+"\\"+f)
+                file_search(regex, face_match, path+"\\"+f, ignore_case, count)
 
 
 list_of_arguments=sys.argv
-#print(list_of_arguments)
+#print(len(list_of_arguments))
 
-if len(list_of_arguments)==1:
-    print("please insert a regex and a file/folder")
+if len(list_of_arguments)!=6:
+    print("please insert all arguments")
     exit()
-elif len(list_of_arguments)==2:
-    print("please insert a file/folder")
-    exit()
-elif list_of_arguments[1]=="not" and type(list_of_arguments[2]) is str:
-    file_search(list_of_arguments[2], False, list_of_arguments[3])
-elif list_of_arguments[1]!="not" and os.path.isfile(list_of_arguments[2]) == False and  os.path.isdir(list_of_arguments[2])==False:
-    file_search(re.compile(list_of_arguments[1]), True, list_of_arguments[2])
-
-elif list_of_arguments[1]!="not" and (os.path.isfile(list_of_arguments[2]) or os.path.isdir(list_of_arguments[2])):
-    file_search(re.compile(list_of_arguments[1]), True, list_of_arguments[2])
-
-
+elif list_of_arguments[1]=="NOT":
+    if list_of_arguments[4]=="-ignoreCase" and list_of_arguments[5]=="-count":
+        file_search(list_of_arguments[2], False, list_of_arguments[3], True, True)
+    elif list_of_arguments[4]=="-ignoreCase" and list_of_arguments[5]!="-count":
+        file_search(list_of_arguments[2], False, list_of_arguments[3], True, False)
+    elif list_of_arguments[4]!="-ignoreCase" and list_of_arguments[5]=="-count":
+        file_search(list_of_arguments[2], False, list_of_arguments[3], False, True)
+    elif list_of_arguments[4]!="-ignoreCase" and list_of_arguments[5]!="-count":
+        file_search(list_of_arguments[2], False, list_of_arguments[3], False, False)
+elif list_of_arguments[1]!="NOT":
+    if list_of_arguments[4]=="-ignoreCase" and list_of_arguments[5]=="-count":
+        file_search(list_of_arguments[2], True, list_of_arguments[3], True, True)
+    elif list_of_arguments[4]=="-ignoreCase" and list_of_arguments[5]!="-count":
+        file_search(list_of_arguments[2], True, list_of_arguments[3], True, False)
+    elif list_of_arguments[4]!="-ignoreCase" and list_of_arguments[5]=="-count":
+        file_search(list_of_arguments[2], True, list_of_arguments[3], False, True)
+    elif list_of_arguments[4]!="-ignoreCase" and list_of_arguments[5]!="-count":
+        file_search(list_of_arguments[2], True, list_of_arguments[3], False, False)
