943fba0d71c2c44797a37804584469e8994eb3d6
Andreea Pădurariu <62242417+Andreeaxhx@users.noreply.github.com>
2020-12-04 00:36:26 +0200

    Update grep.py

--- grep.py
+++ grep.py
@@ -17,47 +17,86 @@ def parser():
 
     return parser
 
-def file_search(pattern, file_path, regex, recursive, files_without_match, count, ignore_case): #regex=expresia regulata;
+def search(pattern, file_path, is_file, regex, recursive, files_without_match, count, ignore_case): #regex=expresia regulata;
                                           #face_match=valoare booleana setata in functie de argumentul NOT;
                                           #path_to_file=path ul catre file ul in care vrem sa se caute regex
     match=False #presupunem ca regex nu face match cu niciun cuvant din file
     nr_of_matches=0
 
     if os.path.isfile(file_path):
-        file=open(file_path)
-        print("-----", file_path)
-        lines=file.readlines() #se citeste file ul linic cu linie
+        file=open(file_path, "r")
+        lines=file.readlines() #se citeste file ul linie cu linie
         for line in lines:
-            if ignore_case:
-                matches_of_regex=re.findall(pattern, line, flags=re.IGNORECASE)
+            if regex:
+                if ignore_case:
+                    matches = re.findall(pattern, line, flags=re.IGNORECASE)
+                else:
+                    matches = re.findall(pattern, line)
+                if matches:
+                    match=True
+                    if files_without_match==False:
+                        for i in matches:
+                            lista=line.split(i, 1)
+                            line=lista[0]+colored(i, 'red')+lista[1]
+                        line=line.replace("\n", "")
+                        if count==False:
+                            if is_file:
+                                print(line)
+                            else:
+                                print(colored(file_path, 'magenta')+":"+line)
+                        nr_of_matches+=1
             else:
-                matches_of_regex = re.findall(pattern, line)
-            if matches_of_regex:
-                match=True
-                if files_without_match:
+                if ignore_case:
+                    pattern_low=pattern.lower()
+                    line_low=line.lower()
 
-                    for i in matches_of_regex:
-                        lista=line.split(i, 1)
-                        line=lista[0]+colored(i, 'red')+lista[1]
-                    line=line.replace("\n", "")
-                    print(line)
-                    nr_of_matches+=1
+                    if line_low.find(pattern_low)>=0:
+                        index = line_low.find(pattern_low)
+                        match=True
+                        nr_of_matches+=1
+                        #line=line.replace(pattern, colored(pattern, 'red'));
+                        line=line[:index]+colored(line[index:index+len(pattern)], 'red')+line[index+len(pattern):]
+                        line=line.replace("\n", '')
+                        if count == False:
+                            if is_file:
+                                print(line)
+                            else:
+                                print(colored(file_path, "magenta")+":"+line)
+                else:
+                    if line.find(pattern)>=0:
+                        match=True
+                        nr_of_matches+=1
+                        line=line.replace(pattern, colored(pattern, "red"))
+                        line=line.replace("\n", '')
+                        if count == False:
+                            if is_file:
+                                print(line)
+                            else:
+                                print(colored(file_path, "magenta")+":"+line)
+
         if count:
-            print(colored("Number of matches:", "green"), nr_of_matches)
-        if match==False and files_without_match==False:
-            print("regex nu face match in file")
+            if is_file:
+                print(nr_of_matches)
+            else:
+                print(colored(file_path, "magenta")+ ":" + str(nr_of_matches))
+        if match==False and files_without_match==True: #!!!!!!!!!!!!!!!! de refacut
+            print(colored(file_path, 'magenta'))
 
-    elif os.path.isdir(file_path):
-
+    elif os.path.isdir(file_path) and recursive:
         for f in os.listdir(file_path):
-
             if os.path.isfile(file_path+"\\"+f):
-                file_search(pattern, file_path+"\\"+f, regex, recursive, files_without_match, count, ignore_case)
-
+                search(pattern, file_path+"\\"+f, False, regex, recursive, files_without_match, count, ignore_case)
+            elif os.path.isdir(file_path+"\\"+f):
+                search(pattern, file_path + "\\" + f, False, regex, recursive, files_without_match, count, ignore_case)
+    elif os.path.isdir(file_path) and not recursive:
+        print("grep: %s: Is a directory" % file_path)
+        if count:
+            print(0)
 
 parser=parser()
 arguments=parser.parse_args()
 pattern=arguments.pattern
 file=arguments.file
+is_file=os.path.isfile(file)
 
-file_search(pattern, file, arguments.reg, arguments.recursive, arguments.files_without_match, arguments.count, arguments.ignore_case)
+search(pattern, file, is_file, arguments.reg, arguments.recursive, arguments.files_without_match, arguments.count, arguments.ignore_case)
